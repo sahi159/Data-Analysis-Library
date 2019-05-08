8030f5b1912535432272cc04507643e11b67f6a5
sainath <smandha1@binghamton.edu>
2019-05-07 21:08:53 -0400

    Project Setup

---
+++ pandas_cub/__init__.py
@@ -0,0 +1,732 @@
+import numpy as np
+
+__version__ = '0.0.1'
+
+
+class DataFrame:
+
+    def __init__(self, data):
+        """
+        A DataFrame holds two dimensional heterogeneous data. Create it by
+        passing a dictionary of NumPy arrays to the values parameter
+
+        Parameters
+        ----------
+        data: dict
+            A dictionary of strings mapped to NumPy arrays. The key will
+            become the column name.
+        """
+        # check for correct input types
+        self._check_input_types(data)
+
+        # check for equal array lengths
+        self._check_array_lengths(data)
+
+        # convert unicode arrays to object
+        self._data = self._convert_unicode_to_object(data)
+
+        # Allow for special methods for strings
+        self.str = StringMethods(self)
+        self._add_docs()
+
+
+    def _check_input_types(self, data):
+        if not isinstance(data, dict):
+            raise TypeError('data  must be a dictionary')
+        for key, value in data.items():
+            if not isinstance(key,str):
+                raise TypeError('Keys of data must be strings')
+            if not isinstance(value,np.ndarray):
+                raise TypeError('Values of data must be numpy arrays')
+            if value.ndim != 1:
+                raise ValueError('Value of data must be one-dimension')
+
+    def _check_array_lengths(self, data):
+        for i,value in enumerate(data.values()):
+            if i==0:
+                length = len(value)
+            elif length != len(value):
+                raise ValueError('All array must be of same length')
+
+    def _convert_unicode_to_object(self, data):
+        new_data = {}
+        for key, value in data.items():
+            if value.dtype.kind == 'U':
+                new_data[key] = value.astype('object')
+            else:
+                new_data[key] = value
+        return new_data
+
+    def __len__(self):
+        return len(next(iter(self._data.values())))
+
+    @property
+    def columns(self):
+        return list(self._data)
+        
+
+    @columns.setter
+    def columns(self, columns):
+        if not isinstance(columns, list):
+            raise TypeError('Columns must be a list')
+        if len(columns) != len(self._data):
+            raise ValueError('New column must be same as the current data')
+        for col in columns:
+            if not isinstance(col, str):
+                raise TypeError('All Column must be strings')
+        if len(columns) != len(set(columns)):
+            raise ValueError('Your column have duplicates .Not allowed')
+        self._data = dict(zip(columns, self._data.values()))
+        
+
+    @property
+    def shape(self):
+
+        return len(self), len(self._data)
+        
+
+    def _repr_html_(self):
+        html = '<table><thead><tr><th></th>'
+        for col in self.columns:
+            html += f"<th>{col:10}</th>"
+
+        html += '</tr></thead>'
+        html += "<tbody>"
+
+        only_head = False
+        num_head = 10
+        num_tail = 10
+        if len(self) <= 20:
+            only_head = True
+            num_head = len(self)
+
+        for i in range(num_head):
+            html += f'<tr><td><strong>{i}</strong></td>'
+            for col, values in self._data.items():
+                kind = values.dtype.kind
+                if kind == 'f':
+                    html += f'<td>{values[i]:10.3f}</td>'
+                elif kind == 'b':
+                    html += f'<td>{values[i]}</td>'
+                elif kind == 'O':
+                    v = values[i]
+                    if v is None:
+                        v = 'None'
+                    html += f'<td>{v:10}</td>'
+                else:
+                    html += f'<td>{values[i]:10}</td>'
+            html += '</tr>'
+
+        if not only_head:
+            html += '<tr><strong><td>...</td></strong>'
+            for i in range(len(self.columns)):
+                html += '<td>...</td>'
+            html += '</tr>'
+            for i in range(-num_tail, 0):
+                html += f'<tr><td><strong>{len(self) + i}</strong></td>'
+                for col, values in self._data.items():
+                    kind = values.dtype.kind
+                    if kind == 'f':
+                        html += f'<td>{values[i]:10.3f}</td>'
+                    elif kind == 'b':
+                        html += f'<td>{values[i]}</td>'
+                    elif kind == 'O':
+                        v = values[i]
+                        if v is None:
+                            v = 'None'
+                        html += f'<td>{v:10}</td>'
+                    else:
+                        html += f'<td>{values[i]:10}</td>'
+                html += '</tr>'
+
+        html += '</tbody></table>'
+        return html
+
+        
+
+    @property
+    def values(self):
+        return np.column_stack(list(self._data.values()))
+
+    @property
+    def dtypes(self):
+        
+        DTYPE_NAME = {'O': 'string', 'i': 'int', 'f': 'float', 'b': 'bool'}
+        col_names = np.array(list(self._data.keys()))
+        dtypes = [DTYPE_NAME[value.dtype.kind] for value in self._data.values()]
+        dtypes = np.array(dtypes)
+        new_data = {'Column Name': col_names, 'Data Type':dtypes}
+        return DataFrame(new_data)
+
+    def __getitem__(self, item):
+
+        if isinstance(item, str):
+            return DataFrame({item: self._data[item]})
+        if isinstance(item, list):
+            return DataFrame({col: self._data[col] for col in item})
+        
+        if isinstance(item, DataFrame):
+            if item.shape[1] != 1:
+                raise ValueError('Can only pass a one column DataFrame for selection')
+
+            arr = next(iter(item._data.values()))
+            if arr.dtype.kind != 'b':
+                raise ValueError('DataFrame must be a boolean')
+
+            new_data = {col: value[arr] for col, value in self._data.items()}
+            return DataFrame(new_data)
+        if isinstance(item, tuple):
+            return self._getitem_tuple(item)
+        raise TypeError("You must pass either a str,list,item or tuple")
+
+    def _getitem_tuple(self, item):
+        # simultaneous selection of rows and cols -> df[rs, cs]
+        if len(item) != 2:
+            raise ValueError('Item tuple must have length 2')
+
+    def _ipython_key_completions_(self):
+        # allows for tab completion when doing df['c
+        pass
+
+    def __setitem__(self, key, value):
+        # adds a new column or a overwrites an old column
+        pass
+
+    def head(self, n=5):
+        """
+        Return the first n rows
+
+        Parameters
+        ----------
+        n: int
+
+        Returns
+        -------
+        DataFrame
+        """
+        pass
+
+    def tail(self, n=5):
+        """
+        Return the last n rows
+
+        Parameters
+        ----------
+        n: int
+        
+        Returns
+        -------
+        DataFrame
+        """
+        pass
+
+    #### Aggregation Methods ####
+
+    def min(self):
+        return self._agg(np.min)
+
+    def max(self):
+        return self._agg(np.max)
+
+    def mean(self):
+        return self._agg(np.mean)
+
+    def median(self):
+        return self._agg(np.median)
+
+    def sum(self):
+        return self._agg(np.sum)
+
+    def var(self):
+        return self._agg(np.var)
+
+    def std(self):
+        return self._agg(np.std)
+
+    def all(self):
+        return self._agg(np.all)
+
+    def any(self):
+        return self._agg(np.any)
+
+    def argmax(self):
+        return self._agg(np.argmax)
+
+    def argmin(self):
+        return self._agg(np.argmin)
+
+    def _agg(self, aggfunc):
+        """
+        Generic aggregation function that applies the
+        aggregation to each column
+
+        Parameters
+        ----------
+        aggfunc: str of the aggregation function name in NumPy
+        
+        Returns
+        -------
+        A DataFrame
+        """
+        pass
+
+    def isna(self):
+        """
+        Determines whether each value in the DataFrame is missing or not
+
+        Returns
+        -------
+        A DataFrame of booleans the same size as the calling DataFrame
+        """
+        pass
+
+    def count(self):
+        """
+        Counts the number of non-missing values per column
+
+        Returns
+        -------
+        A DataFrame
+        """
+        pass
+
+    def unique(self):
+        """
+        Finds the unique values of each column
+
+        Returns
+        -------
+        A list of one-column DataFrames
+        """
+        pass
+
+    def nunique(self):
+        """
+        Find the number of unique values in each column
+
+        Returns
+        -------
+        A DataFrame
+        """
+        pass
+
+    def value_counts(self, normalize=False):
+        """
+        Returns the frequency of each unique value for each column
+
+        Parameters
+        ----------
+        normalize: bool
+            If True, returns the relative frequencies (percent)
+
+        Returns
+        -------
+        A list of DataFrames or a single DataFrame if one column
+        """
+        pass
+
+    def rename(self, columns):
+        """
+        Renames columns in the DataFrame
+
+        Parameters
+        ----------
+        columns: dict
+            A dictionary mapping the old column name to the new column name
+        
+        Returns
+        -------
+        A DataFrame
+        """
+        pass
+
+    def drop(self, columns):
+        """
+        Drops one or more columns from a DataFrame
+
+        Parameters
+        ----------
+        columns: str or list of strings
+
+        Returns
+        -------
+        A DataFrame
+        """
+        pass
+
+    #### Non-Aggregation Methods ####
+
+    def abs(self):
+        """
+        Takes the absolute value of each value in the DataFrame
+
+        Returns
+        -------
+        A DataFrame
+        """
+        return self._non_agg(np.abs)
+
+    def cummin(self):
+        """
+        Finds cumulative minimum by column
+
+        Returns
+        -------
+        A DataFrame
+        """
+        return self._non_agg(np.minimum.accumulate)
+
+    def cummax(self):
+        """
+        Finds cumulative maximum by column
+
+        Returns
+        -------
+        A DataFrame
+        """
+        return self._non_agg(np.maximum.accumulate)
+
+    def cumsum(self):
+        """
+        Finds cumulative sum by column
+
+        Returns
+        -------
+        A DataFrame
+        """
+        return self._non_agg(np.cumsum)
+
+    def clip(self, lower=None, upper=None):
+        """
+        All values less than lower will be set to lower
+        All values greater than upper will be set to upper
+
+        Parameters
+        ----------
+        lower: number or None
+        upper: number or None
+
+        Returns
+        -------
+        A DataFrame
+        """
+        return self._non_agg(np.clip, a_min=lower, a_max=upper)
+
+    def round(self, n):
+        """
+        Rounds values to the nearest n decimals
+
+        Returns
+        -------
+        A DataFrame
+        """
+        return self._non_agg(np.round, decimals=n)
+
+    def copy(self):
+        """
+        Copies the DataFrame
+
+        Returns
+        -------
+        A DataFrame
+        """
+        return self._non_agg(np.copy)
+
+    def _non_agg(self, funcname, **kwargs):
+        """
+        Generic non-aggregation function
+    
+        Parameters
+        ----------
+        funcname: numpy function
+        kwargs: extra keyword arguments for certain functions
+
+        Returns
+        -------
+        A DataFrame
+        """
+        pass
+
+    def diff(self, n=1):
+        """
+        Take the difference between the current value and
+        the nth value above it.
+
+        Parameters
+        ----------
+        n: int
+
+        Returns
+        -------
+        A DataFrame
+        """
+        def func():
+            pass
+        return self._non_agg(func)
+
+    def pct_change(self, n=1):
+        """
+        Take the percentage difference between the current value and
+        the nth value above it.
+
+        Parameters
+        ----------
+        n: int
+
+        Returns
+        -------
+        A DataFrame
+        """
+        def func():
+            pass
+        return self._non_agg(func)
+
+    #### Arithmetic and Comparison Operators ####
+
+    def __add__(self, other):
+        return self._oper('__add__', other)
+
+    def __radd__(self, other):
+        return self._oper('__radd__', other)
+
+    def __sub__(self, other):
+        return self._oper('__sub__', other)
+
+    def __rsub__(self, other):
+        return self._oper('__rsub__', other)
+
+    def __mul__(self, other):
+        return self._oper('__mul__', other)
+
+    def __rmul__(self, other):
+        return self._oper('__rmul__', other)
+
+    def __truediv__(self, other):
+        return self._oper('__truediv__', other)
+
+    def __rtruediv__(self, other):
+        return self._oper('__rtruediv__', other)
+
+    def __floordiv__(self, other):
+        return self._oper('__floordiv__', other)
+
+    def __rfloordiv__(self, other):
+        return self._oper('__rfloordiv__', other)
+
+    def __pow__(self, other):
+        return self._oper('__pow__', other)
+
+    def __rpow__(self, other):
+        return self._oper('__rpow__', other)
+
+    def __gt__(self, other):
+        return self._oper('__gt__', other)
+
+    def __lt__(self, other):
+        return self._oper('__lt__', other)
+
+    def __ge__(self, other):
+        return self._oper('__ge__', other)
+
+    def __le__(self, other):
+        return self._oper('__le__', other)
+
+    def __ne__(self, other):
+        return self._oper('__ne__', other)
+
+    def __eq__(self, other):
+        return self._oper('__eq__', other)
+
+    def _oper(self, op, other):
+        """
+        Generic operator function
+
+        Parameters
+        ----------
+        op: str name of special method
+        other: the other object being operated on
+
+        Returns
+        -------
+        A DataFrame
+        """
+        pass
+
+    def sort_values(self, by, asc=True):
+        """
+        Sort the DataFrame by one or more values
+
+        Parameters
+        ----------
+        by: str or list of column names
+        asc: boolean of sorting order
+
+        Returns
+        -------
+        A DataFrame
+        """
+        pass
+
+    def sample(self, n=None, frac=None, replace=False, seed=None):
+        """
+        Randomly samples rows the DataFrame
+
+        Parameters
+        ----------
+        n: int
+            number of rows to return
+        frac: float
+            Proportion of the data to sample
+        replace: bool
+            Whether or not to sample with replacement
+        seed: int
+            Seeds the random number generator
+
+        Returns
+        -------
+        A DataFrame
+        """
+        pass
+
+    def pivot_table(self, rows=None, columns=None, values=None, aggfunc=None):
+        """
+        Creates a pivot table from one or two 'grouping' columns.
+
+        Parameters
+        ----------
+        rows: str of column name to group by
+            Optional
+        columns: str of column name to group by
+            Optional
+        values: str of column name to aggregate
+            Required
+        aggfunc: str of aggregation function
+
+        Returns
+        -------
+        A DataFrame
+        """
+        pass
+
+    def _add_docs(self):
+        agg_names = ['min', 'max', 'mean', 'median', 'sum', 'var',
+                     'std', 'any', 'all', 'argmax', 'argmin']
+        agg_doc = \
+        """
+        Find the {} of each column
+        
+        Returns
+        -------
+        DataFrame
+        """
+        for name in agg_names:
+            getattr(DataFrame, name).__doc__ = agg_doc.format(name)
+
+
+class StringMethods:
+
+    def __init__(self, df):
+        self._df = df
+
+    def capitalize(self, col):
+        return self._str_method(str.capitalize, col)
+
+    def center(self, col, width, fillchar=None):
+        if fillchar is None:
+            fillchar = ' '
+        return self._str_method(str.center, col, width, fillchar)
+
+    def count(self, col, sub, start=None, stop=None):
+        return self._str_method(str.count, col, sub, start, stop)
+
+    def endswith(self, col, suffix, start=None, stop=None):
+        return self._str_method(str.endswith, col, suffix, start, stop)
+
+    def startswith(self, col, suffix, start=None, stop=None):
+        return self._str_method(str.startswith, col, suffix, start, stop)
+
+    def find(self, col, sub, start=None, stop=None):
+        return self._str_method(str.find, col, sub, start, stop)
+
+    def len(self, col):
+        return self._str_method(str.__len__, col)
+
+    def get(self, col, item):
+        return self._str_method(str.__getitem__, col, item)
+
+    def index(self, col, sub, start=None, stop=None):
+        return self._str_method(str.index, col, sub, start, stop)
+
+    def isalnum(self, col):
+        return self._str_method(str.isalnum, col)
+
+    def isalpha(self, col):
+        return self._str_method(str.isalpha, col)
+
+    def isdecimal(self, col):
+        return self._str_method(str.isdecimal, col)
+
+    def islower(self, col):
+        return self._str_method(str.islower, col)
+
+    def isnumeric(self, col):
+        return self._str_method(str.isnumeric, col)
+
+    def isspace(self, col):
+        return self._str_method(str.isspace, col)
+
+    def istitle(self, col):
+        return self._str_method(str.istitle, col)
+
+    def isupper(self, col):
+        return self._str_method(str.isupper, col)
+
+    def lstrip(self, col, chars):
+        return self._str_method(str.lstrip, col, chars)
+
+    def rstrip(self, col, chars):
+        return self._str_method(str.rstrip, col, chars)
+
+    def strip(self, col, chars):
+        return self._str_method(str.strip, col, chars)
+
+    def replace(self, col, old, new, count=None):
+        if count is None:
+            count = -1
+        return self._str_method(str.replace, col, old, new, count)
+
+    def swapcase(self, col):
+        return self._str_method(str.swapcase, col)
+
+    def title(self, col):
+        return self._str_method(str.title, col)
+
+    def lower(self, col):
+        return self._str_method(str.lower, col)
+
+    def upper(self, col):
+        return self._str_method(str.upper, col)
+
+    def zfill(self, col, width):
+        return self._str_method(str.zfill, col, width)
+
+    def encode(self, col, encoding='utf-8', errors='strict'):
+        return self._str_method(str.encode, col, encoding, errors)
+
+    def _str_method(self, method, col, *args):
+        pass
+
+
+def read_csv(fn):
+    """
+    Read in a comma-separated value file as a DataFrame
+
+    Parameters
+    ----------
+    fn: string of file location
+
+    Returns
+    -------
+    A DataFrame
+    """
+    pass
